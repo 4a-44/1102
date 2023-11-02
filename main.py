0a32bab71aec8009ea18a4ca9f3b95caa3d72661
Jayden Doyle <jaydendonalddoyle@gmail.com>
2023-11-02 16:10:25 -0600

    Finished

---
+++ main.py
@@ -0,0 +1,16 @@
+f = open("three_digit_numbers.txt")
+three_digit_numbers = f.read()
+
+cols = three_digit_numbers.split()
+num_list = []
+for col in cols:
+    num =int(col)
+    num_list.append(num)
+
+start = min(num_list)
+end = max(num_list)
+for count in range(start, end):
+    if count in num_list:
+        pass
+    else:
+        print(f"{count} is missing")
